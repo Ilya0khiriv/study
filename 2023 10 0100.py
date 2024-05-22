5279c1a835b622edf9ef9979e703d58f2ccf5294
Ilya <hirivimsky@gmail.com>
2024-05-22 23:46:29 +0500

    d

---
+++ 2023 10 0100.py
@@ -0,0 +1,10 @@
+def print_params(a, b, c): #Создайте функцию def print_params
+
+    #которая в своем теле будет распечатывать переданное значение из параметра 2 раза!
+    print(a, b, c) # 1 раз
+    print(a, b, c) # 2 раза
+
+
+print_params("lake", "water", "ocean")
+print_params(1, 2, 3)
+print_params([1, 2], {"ffd": "23"}, ("smiling face emoji"))
