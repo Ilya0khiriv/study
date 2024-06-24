6fbf137d3ed1f6797301e2d6ce6880d6509a6ded
Ilya0khiriv <55556801+Ilya0khiriv@users.noreply.github.com>
2024-06-24 15:58:08 +0500

    Update Произвольное число параметров и HTML_DOM.py

--- Произвольное число параметров и HTML_DOM.py
+++ Произвольное число параметров и HTML_DOM.py
@@ -1,19 +1,31 @@
+def get_multiplied_digits(number):
+    number = str(number)
+    digit = int(number[0])
+    
+    if len(number) == 1: return digit
+    
+    stripped_number = number[1:]
+    
+    return digit * get_multiplied_digits(stripped_number)
 
 
-def test(a, *args,  str_ = "Wow", **dict):
-    print(a, str_, args, dict)
-
-test("dsf", 2, 3 ,4,  str_ = "Smiling face emoji", fgf= "run", g = "fly")
+print(get_multiplied_digits(2222))
 
 
-def compute_factorial(n):
+# def test(a, *args,  str_ = "Wow", **dict):
+#     print(a, str_, args, dict)
 
-    if n == 1:
-        return n
+# test("dsf", 2, 3 ,4,  str_ = "Smiling face emoji", fgf= "run", g = "fly")
 
-    else:
-        return n * compute_factorial(n - 1)
 
-r = compute_factorial(5)
-print(r)
+# def compute_factorial(n):
+
+#     if n == 1:
+#         return n
+
+#     else:
+#         return n * compute_factorial(n - 1)
+
+# r = compute_factorial(5)
+# print(r)
 
