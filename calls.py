6a1d89fe164a6f29591a7dcce019a5cf057f8455
Ilya <hirivimsky@gmail.com>
2024-06-25 22:35:46 +0500

    d

---
+++ calls.py
@@ -0,0 +1,29 @@
+calls = 0
+
+
+def count_calls(value):
+    global calls
+    calls += value
+
+def string_info(string):
+    count_calls(1)
+    return (len(string), string.upper(), string.lower())
+
+
+def contains(string, list_to_search):  #is_contians не верно
+    found = False
+    for i in list_to_search:
+        if i.lower() == string.lower():
+            found = True
+
+    if found == True:
+        return True
+    else:
+        return False
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBan
+print(contains('cycle', ['recycle', 'cyclic']))  # No matches
+print(calls)
